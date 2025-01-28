9351a8ffa1860f14f1c3016e42c4e8159ec12c3c
Juan Diego Cardona Mena <15000787@galileo.edu>
2025-01-28 09:03:25 +0000

    First commit

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,19 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+
+#include <iostream>
+
+int main(){
+    std::string user;
+
+    std::cout << "Enter username: " << std::endl;
+    std::cin >> user;
+
+    std::cout << std::endl;
+    std::cout << "Welcome " << user << std::endl << std::endl;
+
+    return(0);
+
+}
